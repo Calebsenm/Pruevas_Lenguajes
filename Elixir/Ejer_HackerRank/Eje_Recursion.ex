8513fe6699aeeaccc583c24dc1b2907104c67c7d
Calebsenm <calebgamercraft@gmail.com>
2022-07-19 19:14:04 -0500

    Está geial

--- Elixir/Ejer_HackerRank/Eje_Recursion.ex
+++ Elixir/Ejer_HackerRank/Eje_Recursion.ex
@@ -2,19 +2,19 @@
 
 defmodule Recursion do 
   def fact(num,tx) when num <= 1 do
-    IO.puts "Hello world"
+    IO.puts tx
   end
 
-  def fact (num,tx) do
-    IO.puts "Hello world"
-    fact ( num - 1,tx)
+  def fact(num,tx) do
+    IO.puts tx
+    fact( num - 1,tx)
   end
 end
 
 nu = IO.gets("Input a number -> ")|> String.trim_trailing
 nu = String.to_integer(nu)
 
-tx = IO.gets "Text -> "
+tx = IO.gets("Input Input a tex -> ")
 Recursion.fact(nu,tx)
 
 
