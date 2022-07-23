a4262a124f55b23f9fe1b58afb49a6db684ac0a2
Calebsenm <calebgamercraft@gmail.com>
2022-07-22 23:49:04 -0500

    Xd

---
+++ Go/progects/httpserver/main.go
@@ -0,0 +1,37 @@
+
+
+package main 
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func getRoot(w http.ResponseWriter, r *http.Request){
+	fmt.Printf("got /request\n")
+	io.WriteString(w, "This is my website!\n")
+}
+
+func getHello(w http.ResponseWriter, r *http.Request){
+	fmt.Printf("got /hello request\n")
+	io.WriteString(w,"hello,HTTP!\n")
+	
+}
+
+func main(){
+	http.HandleFunc("/", getRoot)
+	http.HandleFunc("/", getHello)
+
+	err := http.ListenAndServe(":3333",nill)
+	if errors.Is(err, http.ErrServerClosed){
+		fmt.Printf("Served closed\n")
+
+	}else if err != nill {
+		fmt.Printf("error starting server: %s\n",err)
+		os.Exit(1)
+	}
+
+}
