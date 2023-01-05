c9f6470a0e261ca2e0f7a783477149edc1bed726
Calebsenm <calebgamercraft@gmail.com>
2023-01-05 17:50:10 -0500

    Finish

---
+++ JavaEscript/_2GameRockPaper/index.js
@@ -0,0 +1,58 @@
+
+
+const playerText = document.querySelector("#playerText");
+const computerText = document.querySelector("#computerText");
+const resultText = document.querySelector("#resultText");
+const choiceBtns = document.querySelectorAll(".choiseBtn");
+
+
+
+let player;
+let computer;
+let result; 
+
+choiceBtns.forEach( button => button.addEventListener("click", () => {
+
+    player = button.textContent;
+    computerTurn();
+    
+    playerText.textContent =  `Player: ${player}`;
+    computerText.textContent =  `Computer ${computer}`;
+    resultText.textContent = checkWinner();
+
+
+}));
+
+function computerTurn(){
+    
+    const randNum = Math.floor( Math.random() * 3);
+   
+    switch (randNum){
+    
+        case 1:
+            computer = "Rock";
+            break;
+        case 2:
+            computer = "Paper";
+            break; 
+        case 3:
+            computer = "Scissors";
+            break;
+    
+    }
+
+}
+
+function  checkWinner(){
+
+    if(player == computer){
+        return "Draw!";
+    }   else if(computer == "Rock"){
+            return (player == "Paper") ? "you Win!" : "you Lose"
+    }    else if(computer == "Paper"){
+            return (player == "Scissors") ? "you Win!" : "you Lose"
+    }
+
+}
+
+
